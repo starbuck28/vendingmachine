637db05048d65a498186d5e47dd6aab84247bf28
Elaine Oglesbay <elaineoglesbay@Elaines-MacBook-Pro.local>
2017-08-14 14:54:19 -0400

    wrote first two tests

--- src/main/java/com/elaineoglesbay/VendingMachine.java
+++ src/main/java/com/elaineoglesbay/VendingMachine.java
@@ -1,7 +1,14 @@
 package com.elaineoglesbay;
 
-/**
- * Created by elaineoglesbay on 8/9/17.
- */
+
 public class VendingMachine {
+    private String  balance = "";
+
+    public String display(String balance) {
+        if(balance == ""){
+            return "INSERT COIN";
+        }
+            balance = "1.50";
+        return balance;
+    }
 }
--- src/test/java/VendingMachineTest.java
+++ src/test/java/VendingMachineTest.java
@@ -1,11 +1,19 @@
+import com.elaineoglesbay.VendingMachine;
+import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * Created by elaineoglesbay on 8/9/17.
- */
+
 public class VendingMachineTest {
+
     @Test
     public void itShouldDisplayInsertCoinWhenEmpty() {
+        VendingMachine vendingMachine = new VendingMachine();
+        Assert.assertEquals("INSERT COIN", vendingMachine.display(""));
+    }
 
+    @Test
+    public void itShouldDisplayInsertedAmount() {
+        VendingMachine vendingMachine = new VendingMachine();
+        Assert.assertEquals("1.50", vendingMachine.display("1.50"));
     }
 }
