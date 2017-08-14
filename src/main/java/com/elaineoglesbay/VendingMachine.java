ec2e5611eb1ea2b11c9c7e0860ef624aa0cdb0f5
Elaine Oglesbay <elaineoglesbay@Elaines-MacBook-Pro.local>
2017-08-14 14:31:36 -0400

    Initial commit

---
+++ src/main/java/com/elaineoglesbay/VendingMachine.java
@@ -0,0 +1,7 @@
+package com.elaineoglesbay;
+
+/**
+ * Created by elaineoglesbay on 8/9/17.
+ */
+public class VendingMachine {
+}
---
+++ src/test/java/VendingMachineTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by elaineoglesbay on 8/9/17.
+ */
+public class VendingMachineTest {
+    @Test
+    public void itShouldDisplayInsertCoinWhenEmpty() {
+
+    }
+}
